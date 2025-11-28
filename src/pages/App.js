8e22441a72a2874758f0db08a3e5ad00d1e9b3fd
SaracuzaBr <mateussaracuza@gmail.com>
2025-11-28 14:53:41 -0300

    Mudanças completas.

--- src/pages/App.js
+++ src/pages/App.js
@@ -35,8 +35,7 @@ function App() {
 
   const handleRemoveRepo = (id) => {
     console.log('Removendo registro', id);
-
-    // utilizar filter.
+    setRepos(repos.filter(repo => repo.id !== id));
   }
 
 
@@ -45,7 +44,7 @@ function App() {
       <img src={gitLogo} width={72} height={72} alt="github logo"/>
       <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)} />
       <Button onClick={handleSearchRepo}/>
-      {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
+      {repos.map(repo => <ItemRepo key={repo.id} handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
     </Container>
   );
 }
